e09bf4ed1a58cbd81cdd15e29d02fcaa9cae403c
Mtduffey36 <michael.t.duffey@gmail.com>
2024-05-04 22:22:31 -0500

    starting js local storage

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,5 +1,11 @@
-const themeSwitcher = document.querySelector('#theme-switcher')
+const themeSwitcher = document.querySelector('#theme-switcher');
 const container = document.querySelector('.container');
+const userName = document.getElementById('userbox');
+const title = document.getElementById('titlebox');
+const content = document.getElementById('contentbox');
+// const userName = document.querySelector('userbox');
+// const title = document.querySelector('titlebox');
+// const content = document.querySelector('contentbox');
 let mode = 'dark'
 
 themeSwitcher.addEventListener('click', function () {
@@ -11,7 +17,19 @@ themeSwitcher.addEventListener('click', function () {
       container.setAttribute('class', 'dark');
     }
   });
-  
+
   themeSwitcher.addEventListener('click', function() {
   })
 
+// this is not working atm. 05/04/2024 11:00pm
+  document.getElementById('submitbtn').addEventListener('click', function() {
+    function saveBlog () {
+      const post = {
+        userName: userName.value.trim(),
+        title: title.value.trim(),
+        content: content.value.trim(),
+      };
+      localStorage.setItem('post', JSON.stringify(post));
+      console.log(post);
+    }
+  });
